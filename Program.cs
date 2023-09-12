f9fa4e53da60d75eba414a4c162b812204da6f2a
RockDeaTHeR <rockdeather@mail.ru>
2023-09-12 08:41:59 +0300

    Самое начало...

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}"
+);
 
 app.Run();
